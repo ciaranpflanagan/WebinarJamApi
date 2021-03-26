1daf61ef8338a7be1c1bffaa0bee2e7e1ed2c291
Ciaran Flanagan <ciaran@cicisoft.net>
2021-03-26 20:04:38 +0000

    Inital commit

---
+++ src/WebinarJam.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ciaranpflanagan\WebinarJam;
+
+use Exception;
+
+class LiveStreamService {
+
+    protected $api_key;
+
+    public function __construct(string $key) {
+        $this->api_key = $key;
+    }
+
+    public function webinars () {
+
+        $fields['api_key'] = $this->api_key;
+        $fields_string  = http_build_query($fields);
+
+        try {
+            $ch = curl_init('https://api.webinarjam.com/webinarjam/webinars');
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_POST, count($fields));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+            return json_decode(curl_exec($ch), true);
+        } catch (\Exception $e) {
+
+            throw new Exception($e->getMessage(), 1);
+
+        }
+    }
+}
---
+++ src/WebinarJamServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ciaranpflanagan\WebinarJam;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebinarJamServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
